951eec8ba85ab835f2e3c9fe93115e783190029a
Sliokkory <sliokkory@gmail.com>
2023-09-26 18:49:25 +0300

    Фикс замечаний. Рефактор наименования переменных и функций. Удаление лишнего кода. Изменен порядок блоков кода

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,44 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var count: UILabel!
-    @IBOutlet weak var minusCountBtn: UIButton!
-    @IBOutlet weak var plusCountBtn: UIButton!
-    @IBOutlet weak var clearCountBtn: UIButton!
-    @IBOutlet weak var historyTextArea: UITextView!
-
-    var countLabel = 0
+    @IBOutlet weak private var countLabel: UILabel!
+    @IBOutlet weak private var historyTextArea: UITextView!
+    @IBOutlet weak private var minusCountButton: UIButton!
+    @IBOutlet weak private var plusCountButton: UIButton!
+    @IBOutlet weak private var clearCountButton: UIButton!
+    
+    private var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        minusCountBtn.tintColor = .blue
-        plusCountBtn.tintColor = .red
-        clearCountBtn.tintColor = .black
+        minusCountButton.tintColor = .blue
+        plusCountButton.tintColor = .red
+        clearCountButton.tintColor = .black
     }
     
-    @IBAction func onMinusPress(_ sender: Any) {
-        if countLabel == 0 {
-            historyTextArea.text += "\n\n\(currentDate()) попытка уменьшить значение счётчика ниже 0"
-        } else {
-            historyTextArea.text += "\n\n\(currentDate()) значение изменено на -1"
-            countLabel -= 1
-            count.text = String(countLabel)
-        }
-    }
-    
-    @IBAction func onPlusPress(_ sender: Any) {
-        countLabel += 1
-        count.text = String(countLabel)
-        historyTextArea.text += "\n\n\(currentDate()) значение изменено на +1"
-    }
-    @IBAction func onClearPress(_ sender: Any) {
-        countLabel = 0
-        count.text = String(countLabel)
-        historyTextArea.text += "\n\n\(currentDate()) значение сброшено"
-    }
-    
-    func currentDate() -> String {
+    public func currentDate() -> String {
         let date = Date()
         let calendar = Calendar.current
         let hour = calendar.component(.hour, from: date)
@@ -53,6 +31,28 @@ class ViewController: UIViewController {
         let seconds = calendar.component(.second, from: date)
         let result = "[\(hour):\(minutes):\(seconds)]:"
         return result
+    }
+
+    @IBAction private func minusCountButtonPressed(_ sender: Any) {
+        if count == 0 {
+            historyTextArea.text += "\n\n\(currentDate()) попытка уменьшить значение счётчика ниже 0"
+        } else {
+            historyTextArea.text += "\n\n\(currentDate()) значение изменено на -1"
+            count -= 1
+            countLabel.text = String(count)
+        }
+    }
+
+    @IBAction private func plusCountButtonPressed(_ sender: Any) {
+        count += 1
+        countLabel.text = String(count)
+        historyTextArea.text += "\n\n\(currentDate()) значение изменено на +1"
+    }
+    
+    @IBAction func clearCountButtonPressed(_ sender: Any) {
+        count = 0
+        countLabel.text = String(count)
+        historyTextArea.text += "\n\n\(currentDate()) значение сброшено"
     }
 }
 
